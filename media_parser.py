72cf5c160691c325aaa8c23cd6b0fd54f36acd29
zombyacoff <zombyacoff@gmail.com>
2024-07-08 17:45:20 +0300

    build: update tuparser to 0.1.5.3

--- media_parser.py
+++ media_parser.py
@@ -5,25 +5,19 @@ from tuparser import TelegraphParser, FileManager, TELEGRAPH_URL, run_parser
 
 class MediaParser(TelegraphParser):
     async def parse(self, url, soup):
-        self.main_output_folder = 'output'
-        self.output_folder = 'media'
-        images_folder = 'images'
-        videos_folder = 'videos'
-
-        folder_url = url[19:]
-
-        images = self.get_urls(soup.find_all('img'))
-        videos = self.get_urls(soup.find_all('video'))
+        folder_url = url.split("/")[-1]
+        images = self.get_urls(soup.find_all("img"))
+        videos = self.get_urls(soup.find_all("video"))
 
         if images:
-            await self.download_media(images, images_folder, folder_url, 'gif')
+            await self.download_media(images, "images", folder_url, "gif")
         if videos:
-            await self.download_media(videos, videos_folder, folder_url, 'mp4')
+            await self.download_media(videos, "videos", folder_url, "mp4")
 
     async def download_media(self, media, main_folder, folder, file_extension):
         media_folder_path = FileManager.join_paths(
-            self.main_output_folder,
-            self.output_folder,
+            "output",
+            "media",
             main_folder,
             folder,
         )
@@ -32,20 +26,19 @@ class MediaParser(TelegraphParser):
         for i, value_url in enumerate(media):
             try:
                 async with self.session.get(value_url) as response:
-                    media_file_name = f'{i}.{file_extension}'
-                    media_file_path = FileManager.join_paths(
-                        media_folder_path, media_file_name
-                    )
+                    media_file_name = f"{i+1}.{file_extension}"
+                    media_file_path = FileManager.join_paths(media_folder_path, media_file_name)
                     FileManager.dump_binary_data(media_file_path, await response.read())
             except ClientConnectorError:
-                continue
+                ...
 
     def get_urls(self, media):
         return [
-            TELEGRAPH_URL + value.get('src')
+            TELEGRAPH_URL + value.get("src")
             for value in media
-            if not value.get('src').startswith('http')
+            if not value.get("src").startswith("http")
         ]
 
 
-run_parser(MediaParser)
+if __name__ == "__main__":
+    run_parser(MediaParser)
