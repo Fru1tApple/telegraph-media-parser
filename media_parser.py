eea2764e40e9adac535b027e672269cd354e05f1
zombyacoff <zombyacoff@gmail.com>
2024-07-09 16:26:27 +0300

    refactor: add comment path

--- media_parser.py
+++ media_parser.py
@@ -1,11 +1,10 @@
 from aiohttp.client_exceptions import ClientConnectorError
-
-from tuparser import TelegraphParser, FileManager, TELEGRAPH_URL, run_parser
+from tuparser import TELEGRAPH_URL, FileManager, TelegraphParser, run_parser
 
 
 class MediaParser(TelegraphParser):
     async def parse(self, url, soup):
-        self.folder_url = url.split("/")[-1]
+        self.article = url.split("/")[-1]
         images = self.get_urls(soup.find_all("img"))
         videos = self.get_urls(soup.find_all("video"))
 
@@ -15,11 +14,12 @@ class MediaParser(TelegraphParser):
             await self.download_media(videos, "videos", "mp4")
 
     async def download_media(self, media, media_category, file_extension):
+        # telegraph-media-parser\output\media\{MEDIA_CATEGORY}\{ARTICLE}
         media_folder_path = FileManager.join_paths(
             "output",
             "media",
             media_category,
-            self.folder_url,
+            self.article,
         )
         FileManager.create_folder(media_folder_path)
 
